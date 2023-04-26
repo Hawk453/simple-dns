ed29904c39a30453b4278d9802b4e0c72e0c7d9b
Saksham Madan <sakshammadan@protonmail.com>
2023-04-26 12:32:07 +0530

    ResultCode

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ pub struct BytePacketBuffer {
     pub pos: usize
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum ResultCode {
+    NOERROR = 0,
+    FORMERR = 1,
+    SERVFAIL = 2,
+    NXDOMAIN = 3,
+    NOTIMP = 4,
+    REFUSED = 5,
+}
+
 impl BytePacketBuffer {
     /// Buffer for holding packet contents and to keep track of position.
     pub fn new() -> BytePacketBuffer {
@@ -135,6 +145,19 @@ impl BytePacketBuffer {
     }
 
 
+}
+
+impl ResultCode {
+    pub fn from_num(num: u8) -> ResultCode {
+        match num {
+            1 => ResultCode::FORMERR,
+            2 => ResultCode::SERVFAIL,
+            3 => ResultCode::NXDOMAIN,
+            4 => ResultCode::NOTIMP,
+            5 => ResultCode::REFUSED,
+            0|_ => ResultCode::NOERROR,
+        }
+    }
 }
 
 fn main() {
