929d6534031f0da5323e659b5770eda667dd67aa
Saksham Madan <sakshammadan@protonmail.com>
2023-04-27 16:41:20 +0530

    DNS Record

--- src/main.rs
+++ src/main.rs
@@ -299,6 +299,47 @@ impl DnsQuestion {
     }
 }
 
+impl DnsRecord {
+    pub fn read(buffer: &mut BytePacketBuffer) -> Result<DnsRecord> {
+        let mut domain = String::new();
+        buffer.read_qname(&mut domain)?;
+
+        let qtype_num = buffer.read_u16()?;
+        let qtype = QueryType::from_num(qtype_num);
+        let _ = buffer.read_u16()?;
+        let ttl = buffer.read_u32()?;
+        let data_len = buffer.read_u16()?;
+
+        match qtype {
+            QueryType::A => {
+                let raw_addr = buffer.read_u32()?;
+                let addr = Ipv4Addr::new(
+                    ((raw_addr >> 24) & 0xFF) as u8,
+                    ((raw_addr >> 16) & 0xFF) as u8, 
+                    ((raw_addr >> 8) & 0xFF) as u8, 
+                    ((raw_addr >> 0) & 0xFF) as u8
+                );
+
+                Ok(
+                    DnsRecord::A { 
+                        domain, addr, ttl 
+                    }
+                )
+            }
+
+            QueryType::UNKNOWN(_) => {
+                buffer.step(data_len as usize)?;
+
+                Ok(
+                    DnsRecord::UNKNOWN { 
+                        domain, qtype: qtype_num, data_len, ttl
+                    }
+                )
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
